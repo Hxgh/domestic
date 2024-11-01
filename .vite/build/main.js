d8b1a5af30a18f81b001ce056cbe1782b7eb2e61
阳光宝宝 <3088816598@qq.com>
2024-11-01 16:53:25 +0800

    chore

---
+++ .vite/build/main.js
@@ -0,0 +1,24 @@
+"use strict";
+const electron = require("electron");
+const path = require("path");
+const createWindow = () => {
+  const mainWindow = new electron.BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js")
+    }
+  });
+  mainWindow.loadURL("http://192.168.102.167:8200/");
+};
+electron.app.on("ready", createWindow);
+electron.app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    electron.app.quit();
+  }
+});
+electron.app.on("activate", () => {
+  if (electron.BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
