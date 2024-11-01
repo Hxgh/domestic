d791b5f7d519ef595a80af00deb496e82196970d
阳光宝宝 <3088816598@qq.com>
2024-11-01 16:14:17 +0800

    chore

--- forge.config.ts
+++ forge.config.ts
@@ -15,7 +15,7 @@ const config = {
   rebuildConfig: {},
   makers: [
     new MakerSquirrel({}),
-    new MakerZIP({}, ['darwin', 'linux']),
+    new MakerZIP({}, ['linux']),
     new MakerRpm({}),
     new MakerDeb({}),
   ],
