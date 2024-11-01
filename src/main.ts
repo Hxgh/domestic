ee22b310ca5a7895b90f5d62e433c2cdeb1a1b30
阳光宝宝 <3088816598@qq.com>
2024-11-01 15:46:33 +0800

    chore

---
+++ src/main.ts
@@ -0,0 +1,34 @@
+import { app, BrowserWindow } from 'electron';
+import path from 'path';
+
+// if (require('electron-squirrel-startup')) {
+//   app.quit();
+// }
+
+const createWindow = () => {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
+
+  mainWindow.loadURL('http://192.168.102.167:8200/');
+
+  // mainWindow.webContents.openDevTools();
+};
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
